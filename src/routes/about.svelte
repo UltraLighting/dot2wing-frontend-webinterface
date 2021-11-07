02ab40d5d3a65c1b630f7acdce779519d99c5087
Phil <philip.schaefer0@gmail.com>
2021-11-07 19:56:13 +0100

    added basic websites and tailwind support

---
+++ src/routes/about.svelte
@@ -0,0 +1,2 @@
+<h1>About</h1>
+<p>This is the dashboard for the faderwing by ultraLighting used with dot2 or grandMA2. </p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-4xl text-center my-8 uppercase">Welcome to ultralighing</h1>
+<p>This is the dashboard for the fader wing</p>
