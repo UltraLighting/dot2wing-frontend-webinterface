445f8c347d12b1f9f776fcbdbe343d75771349ef
Phil <philip.schaefer0@gmail.com>
2021-11-09 23:23:57 +0100

    added testpage for styles

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,4 +1,5 @@
-<nav>
-  <a href="/">Home</a>
-  <a href="/about">About</a>
+<nav class="my-4 flex justify-center w-full">
+  <a class="mx-4 text-lg" href="/">Home</a>
+  <a class="mx-4 text-lg" href="/about">About</a>
+  <a class="mx-4 text-lg" href="/test">test</a>
 </nav>
