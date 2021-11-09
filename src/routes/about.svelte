4164be29764c972bc69011584922412efa182ea6
Phil <philip.schaefer0@gmail.com>
2021-11-09 23:24:19 +0100

    updated formatting

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,2 +1,2 @@
-<h1>About</h1>
+<h1 class="text-4xl text-center my-8 uppercase">About</h1>
 <p>This is the dashboard for the faderwing by ultraLighting used with dot2 or grandMA2. </p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,2 @@
-<h1 class="text-4xl text-center my-8 uppercase">Welcome to ultralighing</h1>
+<h1 class="text-4xl text-center my-8 uppercase">Welcome to ultralighting</h1>
 <p>This is the dashboard for the fader wing</p>
