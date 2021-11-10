fbe5ff81de98276758974653386cdf3e7368571f
Phil <philip.schaefer0@gmail.com>
2021-11-10 22:41:08 +0100

    fixed spelling mistake

--- src/components/version.svelte
+++ src/components/version.svelte
@@ -1,4 +1,4 @@
 <div class="text-center text-gray-500 text-xs">
   <p>v.3.0.0</p>
-  <p class="mt-3">&copy;2021 unltraLighting. All rights reserved.</p>
+  <p class="mt-3">2021 ultraLighting. All rights reserved.</p>
 </div>
