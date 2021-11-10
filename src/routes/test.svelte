502b82270b7eaaa0765b1a3252a8b33d72b0bc9d
Phil <philip.schaefer0@gmail.com>
2021-11-10 22:05:53 +0100

    deleted

--- src/routes/test.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  import SettingWindow from '../components/settingWindow.svelte';
-</script>
-<h1 class="text-4xl text-center my-8 uppercase">Style check</h1>
-<div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
-  <SettingWindow/>
-</div>
