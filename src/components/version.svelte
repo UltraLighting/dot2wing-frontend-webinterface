a78085de2462978336fe1898bfcfff93b7cdc8df
Phil <philip.schaefer0@gmail.com>
2021-11-10 18:47:00 +0100

    added version comp

---
+++ src/components/version.svelte
@@ -0,0 +1,3 @@
+<p class="text-center text-gray-500 text-xs">
+  &copy;2021 unltraLighting. All rights reserved. v. 3.0.0
+</p>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,11 @@
+<script>
+  import Version from '../components/version.svelte';
+</script>
 <svelte:head>
   <title>Wing Dashboard</title>
 </svelte:head>
 <slot></slot>
+<Version/>
 <style>
   @tailwind base;
   @tailwind components;
