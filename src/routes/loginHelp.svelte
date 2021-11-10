1d041a0b4f54d0d3a101a7c2fce2969d7f912d19
Phil <philip.schaefer0@gmail.com>
2021-11-10 22:06:01 +0100

    inital init

---
+++ src/routes/loginHelp.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Nav from '../components/nav.svelte';
+</script>
+<Nav title="forgot password" loggedOut=true />
+<!-- help page -->
+<div class="flex h-auto mt-11">
+  <div class="m-auto mb-6 bg-black shadow-md rounded-md px-8 pt-6 pb-8">
+    <div class="text-gray-400">
+      <p>Once you open up the dashboard, your wing will display a PIN on the display. Use that to log in.</p>
+      <p>Use that to log in.</p>
+    </div>
+    <div class="mt-5 flex items-center justify-between">
+      <a href="/" class="py-2 px-2 font-medium text-white bg-gray-400 rounded hover:bg-gray-700 transition duration-200 uppercase">bring me back</a>
+    </div>
+  </div>
+</div>
