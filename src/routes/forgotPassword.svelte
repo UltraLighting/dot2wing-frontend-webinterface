8ed09f4f32ba47e51453e63e892ec4244ed02396
Phil <philip.schaefer0@gmail.com>
2021-11-10 18:47:17 +0100

    added forget password page

---
+++ src/routes/forgotPassword.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Nav from '../components/nav.svelte';
+</script>
+<!-- TODO: change loggedout-value in production -->
+<Nav title="Welcome to your wing" loggedOut=true/>
+<!-- login page -->
+<div class="w-full max-w-xs bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 justify-center">
+  <p>test</p>
+</div>
