43d74f06da60779b31df74b1ff6eac90fa75dbb9
Phil <philip.schaefer0@gmail.com>
2021-11-10 22:41:29 +0100

    cleanup and design change

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,9 @@
 <script>
   import Nav from '../components/nav.svelte';
 </script>
-<Nav title="Welcome to your wing" loggedOut=true/>
+<Nav title="Welcome to your wing!" loggedOut=true/>
 <!-- login page -->
-<div class="flex w-full mt-11">
+<div class="flex w-full mt-40">
   <form class="m-auto bg-black shadow-md rounded-md px-8 pt-6 pb-8 mb-6">
     <div class="mb-6">
       <label class="block text-gray-400 text-sm font-bold mb-2 uppercase" for="password">pin</label>
@@ -15,15 +15,9 @@
       </div>
         <p class="text-red-500 text-xs italic">Wrong PIN.</p>
     </div>
-    <div class="flex items-center justify-between">
-      <!-- <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline uppercase" type="button">
+    <div class="flex justify-center">
+      <a class="bg-gray-400 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded uppercase" href="/dashboard">
         Login
-      </button> -->
-      <a class="bg-gray-400 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline uppercase" href="/dashboard">
-        Login
-      </a>
-      <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="/loginHelp">
-        How does this work?
       </a>
     </div>
   </form>
